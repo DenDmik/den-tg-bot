13dc530edf818df884d8d742dd55ef6f8077e0ab
DenDmik <dmikdenis@gmail.com>
2025-04-03 11:38:56 +0000

    18

--- api/index1.js
+++ api/index1.js
@@ -14,9 +14,14 @@ const bot = new TelegramBot(token,{
  bot.setWebHook(`https://den-tg-bot.vercel.app/bot${token}`)
 
 
-
+ const start = async()=>{
+    bot.setMyCommands([
+        {command: '/start', description: 'Начальное приветствие'},
+        {command: '/info', description: 'Получить информацию о пользователе'},
+        {command: '/music', description: 'Музыка'},
+    ])}
       
-   
+   start()
     
     bot.on('message',async (msg) => {
         console.log(msg.from.username)
@@ -26,10 +31,11 @@ const bot = new TelegramBot(token,{
       
     if(msg.text === '/info') {
       bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
-    } else{ bot.sendSticker(chatId,'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')}
+    }
      
     if(msg.text === '/music'){
-        bot.sendAudio(chatId,'https://muz8.z3.fm/1/50/dskarlatti_-_sonata_b-moll__k27_l449_(zf.fm).mp3?download=force')
+     await  bot.sendAudio(chatId,'https://muz8.z3.fm/1/50/dskarlatti_-_sonata_b-moll__k27_l449_(zf.fm).mp3?download=force')
     }
+    if(msg.text ==='/start'){bot.sendSticker(chatId,'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')}
     
 })
