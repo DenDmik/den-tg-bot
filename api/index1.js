12ab7827dc56f59026cc136c216f3ba64cb7bd81
DenDmik <dmikdenis@gmail.com>
2025-04-04 16:00:22 +0000

    24

--- api/index1.js
+++ api/index1.js
@@ -24,14 +24,14 @@ const bot = new TelegramBot(token,{
    start()
 
     
-    bot.on('message',async (msg) => {
+    bot.on('message', (msg) => {
         console.log(msg.from.username)
         const text = msg.text
       const chatId = msg.chat.id;
 
      if(msg.text ==='/start'){
-     await bot.sendSticker(chatId,'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')
-     await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+      bot.sendSticker(chatId,'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')
+      bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
         reply_markup: {
             inline_keyboard: [
                 [{text: 'Перейти в Next-deploy project', web_app: {url: webAppUrl}}]
@@ -47,6 +47,6 @@ const bot = new TelegramBot(token,{
     }
      
     if(msg.text === '/music'){
-     await  bot.sendAudio(chatId,'https://muz8.z3.fm/1/50/dskarlatti_-_sonata_b-moll__k27_l449_(zf.fm).mp3?download=force')
+       bot.sendAudio(chatId,'https://muz8.z3.fm/1/50/dskarlatti_-_sonata_b-moll__k27_l449_(zf.fm).mp3?download=force')
     }
    })
